13ff809c96728077d5426ffeb4f648549b462931
miodas <wielkimiodas@gmail.com>
2013-11-08 01:35:10 +0100

    postgres writer project created

---
+++ source/Yandex.PostgresWriter/DbConnector.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace Yandex.PostgresWriter
+{
+    public class DbConnector
+    {
+        private readonly string _server = "localhost";
+        private readonly string _userId = "postgres";
+        private readonly string _port = "5432";
+        private readonly string _password = "password";
+        private readonly string _database = "yandex";
+        private NpgsqlConnection _connection;
+
+        public DbConnector()
+        {
+            
+        }
+
+        public DbConnector(string server, string userId, string port, string password, string database)
+        {
+            this._server = server;
+            this._userId = userId;
+            this._port = port;
+            this._password = password;
+            this._database = database;
+        }
+
+        private void Connect()
+        {
+            var connstring = String.Format("Server={0};Port={1};" + "User Id={2};Password={3};Database={4};", _server, _port, _userId, _password, _database);
+            _connection = new NpgsqlConnection(connstring);
+            _connection.Open();
+        }
+
+        private void Disconnect()
+        {
+            _connection.Close();
+        }
+
+        public DataSet ExecuteQuery(string query)
+        {
+            Connect();
+            
+            var dataAdapter = new NpgsqlDataAdapter(query, _connection);
+            var dataSet = new DataSet();
+            dataAdapter.Fill(dataSet);
+            
+            Disconnect();
+            
+            return dataSet;
+        }
+    }
+}
---
+++ source/Yandex.UnitTests/PostgrasWriterTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Yandex.PostgresWriter;
+
+namespace Yandex.UnitTests
+{
+    [TestClass]
+    public class PostgrasWriterTest
+    {
+        [TestMethod]
+        public void QueryExecutionTest()
+        {
+            var connector = new DbConnector();
+            var result = connector.ExecuteQuery("SELECT column1 FROM testschema.\"testTable\";");
+            var table = result.Tables[0];
+
+            foreach (var dbRow in table.Rows.Cast<DataRow>())
+            {
+                Debug.WriteLine(dbRow[0]);
+            }
+        }
+    }
+}
