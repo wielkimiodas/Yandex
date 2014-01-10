2ec2098d80632ad711e6719cb729d09b167f1281
miodas <wielkimiodas@gmail.com>
2014-01-10 22:42:29 +0100

    coprimes - not working yet

---
+++ source/Yandex.CoPrimes/CoPrime.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Yandex.Coprimes
+{
+    public class CoPrime
+    {
+        public static Tuple<long, long> GetCoprimeTuple()
+        {
+            var random = new Random();
+            const long terms = 10000; //minhash.getparams
+            long first = random.Next(100, 1000000);
+
+            if (first%2 == terms%2) first++;
+
+            while (!IsPairCoPrime(first, terms))
+            {
+                first+=2;
+            }
+            long second = first + 1;
+            while (!IsPairCoPrime(first, second))
+            {
+                second+=2;
+            }
+            return new Tuple<long, long>(first, second);
+        }
+
+        public static bool IsPairCoPrime(long a, long b)
+        {
+            return GCD(a, b) == 1;
+        }
+
+        public static long GCD(long a, long b)
+        {
+            while (b != 0)
+            {
+                long t = b;
+                b = a % b;
+                a = t;
+            }
+            return a;
+        }
+    }
+}
---
+++ source/Yandex.CoPrimes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace Yandex.Coprimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var watch = Stopwatch.StartNew();
+            for(int i=0;i<10000;i++)
+            {
+                var o = CoPrime.GetCoprimeTuple();
+                Console.WriteLine(o.Item1 + " " + o.Item2);
+            }
+            watch.Stop();
+            Console.WriteLine("took: "+ watch.Elapsed.TotalSeconds);
+            Console.ReadKey();
+
+        }
+    }
+}
